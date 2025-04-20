4c8630ac7466c457562612c0e8a4d36ff7981f5c
joshuamumbere <joshuamumbere71@gmail>
2025-04-21 02:53:24 +0300

    Added the fonts and new screens to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/splash_screen.dart';
 import 'screens/onboarding_screen.dart';
+import 'screens/roleselectionscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,10 +21,15 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const SplashScreen(),
         '/onboarding': (context) => const OnboardingScreen(),
+        '/role-select': (context) => RoleSelectionScreen(),
+        '/client-onboarding': (context) => ClientOnboardingScreen(),
+        '/lawyer-verification': (context) => LawyerVerificationScreen(),
+        '/browse': (context) => GuestBrowseScreen(),
       },
 
       // Optional: Custom theme
       theme: ThemeData(
+        fontFamily: 'Euclid',
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
